bb08c15388fd5b97e1cd7bf6888de093474dd1a7
Ravi kiran Reddy Chinta <74817764+Ravikiran-tech@users.noreply.github.com>
2022-07-23 03:21:48 +0530

    Add files via upload

---
+++ html_download.py
@@ -0,0 +1,25 @@
+import os
+import time
+import requests
+import sys
+
+
+def code_html():
+    for year in range(2013,2019):
+        for month in range(1,13):
+            if(month<10):
+                url='http://en.tutiempo.net/climate/0{}-{}/ws-421820.html'.format(month,year)  #for month 1-9
+            else:
+                url='http://en.tutiempo.net/climate/{}-{}/ws-421820.html'.format(month,year)    #for months 10-12
+            texts=requests.get(url)
+            text_utf=texts.text.encode('utf=8')
+            
+            if not os.path.exists("Data/Html_Data/{}".format(year)):
+                os.makedirs("Data/Html_Data/{}".format(year))
+            with open("Data/Html_Data/{}/{}.html".format(year,month),"wb") as output:
+                output.write(text_utf)
+            
+        sys.stdout.flush()
+
+code_html()
+        
